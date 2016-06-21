dbaedac1fe045a8c171d150a66c3fc76d0c8f4ee
rajeev021 <rajeev.p@ivoice.in>
2016-06-21 13:16:00 +0530

    formatting

--- SwiftVersionControl/ViewController.swift
+++ SwiftVersionControl/ViewController.swift
@@ -15,44 +15,44 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("first branch")
+        
         print("first branch2")
-
-       print("My new changes in mybranch2")
-
-       print("The New Changes in new branchesssss")
-
-   self.someLabel.text = "xyz"
-
+        
+        print("My new changes in mybranch2")
+        
+        print("The New Changes in new branchesssss")
+        
+        self.someLabel.text = "xyz"
+        
         print("My branch 2")
+        
         print("My branch 3 new changes are done")
-
+        
         print("My Branch 3")
-         print("first branch")
-
+        
+        print("first branch")
         
         print("My branch nagarjuna")
-
-
-         print("My Branch 4 is yet to created")
-
+        
+        print("My Branch 4 is yet to created")
+        
         print("My Branch 4 is yet to created")
         
         print("am back with my commits, lol ")
-
+        
         
         print(" rajeev committed ");
-
-         print("My branch 6")
         
+        print("My branch 6")
         
         print("Testing Revert");
         
         print("Master changes")
-
+        
         
 }
 
-    override func didReceiveMemoryWarning() {
+override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
